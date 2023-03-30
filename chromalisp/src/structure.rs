e6afd846832a9e467fe11da0a4743eee595be286
p6nj <breee@duck.com>
2023-03-30 10:39:25 +0200

    More wrappers, Slide enum for legato variant

--- chromalisp/src/structure.rs
+++ chromalisp/src/structure.rs
@@ -2,6 +2,12 @@ pub enum Item{
     Note(u8),
     Silence
 }
+
+pub enum Slide{
+    Static(u16),
+    Dynamic(u8)
+}
+
 pub enum Wrapper{
     Song(String, Vec<Wrapper>),
     Album(String, Vec<Wrapper>),
@@ -9,6 +15,10 @@ pub enum Wrapper{
     Year(u16, Vec<Wrapper>),
     NoteDef(Vec<String>, Vec<Wrapper>),
     Channel(Vec<Wrapper>),
+    Length(u8, Vec<Wrapper>),
+    Octave(u8, Vec<Wrapper>),
+    Loop(u8, Vec<Wrapper>),
+    Legato{start:Slide, end:Slide},
     Singleton(Item)
     //TODO: finish this enum
 }
