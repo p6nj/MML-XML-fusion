1ed487a0d1b9ecd8e12c1dadaa8540c34b4e1133
p6nj <breee@duck.com>
2023-06-21 12:14:11 +0200

    no more parsers, working xml to html

--- xmlmml/src/lib.rs
+++ xmlmml/src/lib.rs
@@ -1,8 +1,6 @@
 #[allow(unused, dead_code)]
 mod files;
 #[allow(unused, dead_code)]
-mod parsers;
-#[allow(unused, dead_code)]
 mod structure;
 #[allow(unused, dead_code)]
 mod xslt;
@@ -17,13 +15,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
     fn xslt() {
-        xslt::xslt2html();
+        assert_eq!("<html><head><title>XSLT in Rust</title></head><body><p>A simple document.</p></body></html>".to_string(),xslt::xml2html("<Example><Title>XSLT in Rust</Title><Paragraph>A simple document.</Paragraph></Example>","<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
+        <xsl:template match='child::Example'><html><xsl:apply-templates/></html></xsl:template>
+        <xsl:template match='child::Title'><head><title><xsl:apply-templates/></title></head></xsl:template>
+        <xsl:template match='child::Paragraph'><body><p><xsl:apply-templates/></p></body></xsl:template>
+      </xsl:stylesheet>").unwrap());
     }
 }
--- xmlmml/src/xslt.rs
+++ xmlmml/src/xslt.rs
@@ -1,36 +1,28 @@
-pub fn xslt2html() {
-    use std::rc::Rc;
-    use xrust::evaluate::{Evaluator, StaticContext};
-    use xrust::intmuttree::{Document, NodeBuilder, RNode};
-    use xrust::item::{Item, Node, NodeType, Sequence, SequenceTrait};
-    use xrust::qname::QualifiedName;
-    use xrust::xdmerror::Error;
-    use xrust::xslt::from_document;
+use std::rc::Rc;
+use xrust::evaluate::{Evaluator, StaticContext};
+use xrust::intmuttree::{Document, NodeBuilder, RNode};
+use xrust::item::{Item, Node, NodeType, Sequence, SequenceTrait};
+use xrust::qname::QualifiedName;
+use xrust::xdmerror::Error;
+use xrust::xslt::from_document;
 
-    // A little helper function that wraps the toplevel node in a Document
-    fn make_from_str(s: &str) -> Result<RNode, Error> {
-        let e = Document::try_from(s).expect("failed to parse XML").content[0].clone();
-        let mut d = NodeBuilder::new(NodeType::Document).build();
-        d.push(e).expect("unable to append node");
-        Ok(d)
-    }
+// A little helper function that wraps the toplevel node in a Document
+fn make_from_str(s: &str) -> Result<RNode, Error> {
+    let e = Document::try_from(s)?.content[0].clone();
+    let mut d = NodeBuilder::new(NodeType::Document).build();
+    d.push(e)?;
+    Ok(d)
+}
 
+pub fn xml2html(xml: &str, xsl: &str) -> Result<String, Error> {
     // First setup a static context for the evaluator
     let mut sc = StaticContext::new_with_builtins();
 
     // The source document (a tree)
-    let src = Rc::new(Item::Node(
-    make_from_str("<Example><Title>XSLT in Rust</Title><Paragraph>A simple document.</Paragraph></Example>")
-    .expect("unable to parse XML")
-));
+    let src = Rc::new(Item::Node(make_from_str(xml)?));
 
     // The XSL stylesheet
-    let style = make_from_str("<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
-  <xsl:template match='child::Example'><html><xsl:apply-templates/></html></xsl:template>
-  <xsl:template match='child::Title'><head><title><xsl:apply-templates/></title></head></xsl:template>
-  <xsl:template match='child::Paragraph'><body><p><xsl:apply-templates/></p></body></xsl:template>
-</xsl:stylesheet>")
-    .expect("unable to parse stylesheet");
+    let style = make_from_str(xsl).expect("unable to parse stylesheet");
 
     // Compile the stylesheet
     let ev = from_document(style, &mut sc, None, make_from_str, |_| Ok(String::new()))
@@ -47,7 +39,7 @@ pub fn xslt2html() {
 
     // Let 'er rip!
     // Evaluate the sequence constructor with the source document as the initial context
-    let seq = ev
-        .evaluate(Some(vec![Rc::clone(&src)]), Some(0), &t, &rd)
-        .expect("evaluation failed");
+    Ok(ev
+        .evaluate(Some(vec![Rc::clone(&src)]), Some(0), &t, &rd)?
+        .to_xml())
 }
