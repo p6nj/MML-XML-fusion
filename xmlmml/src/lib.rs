4c59a17add60e0992ab5974223f0ae4f8af96571
p6nj <breee@duck.com>
2023-06-22 14:52:51 +0200

    keep static xsl, remove more expect

--- xmlmml/src/lib.rs
+++ xmlmml/src/lib.rs
@@ -16,10 +16,6 @@ mod tests {
 
     #[test]
     fn xslt() {
-        assert_eq!("<html><head><title>XSLT in Rust</title></head><body><p>A simple document.</p></body></html>".to_string(),xslt::xml2html("<Example><Title>XSLT in Rust</Title><Paragraph>A simple document.</Paragraph></Example>","<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
-        <xsl:template match='child::Example'><html><xsl:apply-templates/></html></xsl:template>
-        <xsl:template match='child::Title'><head><title><xsl:apply-templates/></title></head></xsl:template>
-        <xsl:template match='child::Paragraph'><body><p><xsl:apply-templates/></p></body></xsl:template>
-      </xsl:stylesheet>").unwrap());
+        assert_eq!("<html><head><title>XSLT in Rust</title></head><body><p>A simple document.</p></body></html>".to_string(),xslt::xml2html("<Example><Title>XSLT in Rust</Title><Paragraph>A simple document.</Paragraph></Example>").unwrap());
     }
 }
--- xmlmml/src/xslt.rs
+++ xmlmml/src/xslt.rs
@@ -14,7 +14,7 @@ fn make_from_str(s: &str) -> Result<RNode, Error> {
     Ok(d)
 }
 
-pub fn xml2html(xml: &str, xsl: &str) -> Result<String, Error> {
+pub fn xml2html(xml: &str) -> Result<String, Error> {
     // First setup a static context for the evaluator
     let mut sc = StaticContext::new_with_builtins();
 
@@ -22,20 +22,21 @@ pub fn xml2html(xml: &str, xsl: &str) -> Result<String, Error> {
     let src = Rc::new(Item::Node(make_from_str(xml)?));
 
     // The XSL stylesheet
-    let style = make_from_str(xsl).expect("unable to parse stylesheet");
+    let style = make_from_str("<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
+    <xsl:template match='child::Example'><html><xsl:apply-templates/></html></xsl:template>
+    <xsl:template match='child::Title'><head><title><xsl:apply-templates/></title></head></xsl:template>
+    <xsl:template match='child::Paragraph'><body><p><xsl:apply-templates/></p></body></xsl:template>
+    </xsl:stylesheet>")?;
 
     // Compile the stylesheet
-    let ev = from_document(style, &mut sc, None, make_from_str, |_| Ok(String::new()))
-        .expect("failed to compile stylesheet");
+    let ev = from_document(style, &mut sc, None, make_from_str, |_| Ok(String::new()))?;
 
     // Make an empty result document
     let rd = NodeBuilder::new(NodeType::Document).build();
 
     // Prime the stylesheet evaluation by finding the template for the document root
     // and making the document root the initial context
-    let t = ev
-        .find_match(&src, None, &rd)
-        .expect("unable to find match");
+    let t = ev.find_match(&src, None, &rd)?;
 
     // Let 'er rip!
     // Evaluate the sequence constructor with the source document as the initial context
